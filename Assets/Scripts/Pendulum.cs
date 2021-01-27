4287d636d4af64dedb06f0f3cc645843b7181a7c
Lelio <lelio.rollin@e-artsup.net>
2021-01-27 11:30:35 +0100

    Probuilder

---
+++ Assets/Scripts/Pendulum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    private Quaternion start, end;
+
+    [SerializeField, Range(0.0f, 360f)] 
+    private float angle = 90f;
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
