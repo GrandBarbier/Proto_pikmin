a53a8d1e36da0999d21e326732ca6537a2f77717
Gryfs <Allan18040@gmail.com>
2021-01-28 10:52:25 +0100

    Push texture new scene

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = rb.position;
+        rb.position += Vector3.back * speed * Time.fixedDeltaTime;
+        rb.MovePosition(pos);    
+    }
+}
