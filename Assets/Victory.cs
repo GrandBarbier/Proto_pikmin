8daedf8a0d0c6e016da0b812d5a0378c6ec32e65
Lelio <lelio.rollin@e-artsup.net>
2021-01-28 10:14:22 +0100

    Death

--- Assets/Victory.cs
+++ Assets/Victory.cs
@@ -1,40 +1,52 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 public class Victory : MonoBehaviour
 {
+    public MultipleTargetCam targetsCam;
+    
     public List<GameObject> targets;
-    public Text victory;
-    public Text Score;
+    
+    public GameObject victory;
+    public GameObject lose;
+    
+    
+    public TextMeshProUGUI Score;
 
     public float score;
-    // Start is called before the first frame update
+    public float scoreMin;
+    
     void Start()
     {
         foreach (GameObject players in GameObject.FindGameObjectsWithTag("Player") )
         {
             targets.Add(players);
         }
+
+        victory.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         score = score + 1;
         targets.Remove(other.gameObject);
+        targetsCam.targets.Remove(other.transform);
         Destroy(other.gameObject);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (targets.Count <= 0)
         {
-            victory.enabled = true;
-            Score.text = score.ToString();
-
+            victory.SetActive(true);
         }
+        
+        Score.text = "Score : " + score;
     }
+    
 }
