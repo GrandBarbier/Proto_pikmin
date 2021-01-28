af72bbb80918cb98d21de2efddebdc2e4e6646b6
Lelio <lelio.rollin@e-artsup.net>
2021-01-28 15:21:00 +0100

    Start hydraulic press

---
+++ Assets/Scripts/HydraulicPress.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HydraulicPress : MonoBehaviour
+{
+    public float amplitude;
+    
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
