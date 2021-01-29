0fb1f2b440d50f18520b88729c19fbe44a250266
Lelio <lelio.rollin@e-artsup.net>
2021-01-29 13:43:23 +0100

    import audio clips

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource move;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
